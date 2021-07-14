3e6cfa8b4b4fd81cd1bac4d210f3c6fa1d4c28a9
AlirezaEnergy <aghadertootoonchi@gmail.com>
2021-07-14 17:17:05 +0430

    v1 is created.

--- num_guess.py
+++ num_guess.py
@@ -1,6 +1,21 @@
 import numpy as np
 
 LowerBound = 0
-UpperBound = 0
+UpperBound = 10
 
-Rand = np.random.randint(LowerBound,UpperBound)
+Rand = np.random.randint(LowerBound,UpperBound+1)
+
+MaxGuess = 5
+for i in range(1,MaxGuess+1):
+    
+    print(f'You have {MaxGuess+1-i} chance(s) left!!')
+    guess = int(input('Enter an integer between 0 and 10: '))
+    
+    if i == MaxGuess:
+        print('You lost.')
+    else:
+        if guess == Rand:
+            print('You WON!!')
+            break
+        else:
+            print('Incorrect guess.')
